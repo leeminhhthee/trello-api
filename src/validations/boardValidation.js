c7186cf432c681208f5effa0daf3b570d90babc9
leeminhhthee <minhthe22122003@gmail.com>
2025-03-30 00:02:15 +0700

    error_handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -14,20 +15,12 @@ const createNew = async (req, res, next) => {
   })
 
   try {
-    console.log('req.body: ', req.body)
-    console.log('req.query: ', req.query)
-    console.log('req.params: ', req.params)
-    console.log('req.files: ', req.files)
-
     // Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về all lỗi
     await correctCondition.validateAsync(req.body, { abortEarly: false })
     // Validate dữ liệu xong, hợp lệ thì cho request đi tiếp sang Controller/Middleware
     next()
   } catch (error) {
-    // console.log(error)
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 
 }
