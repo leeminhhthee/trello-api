3043fd0e8851b138f85ac2c587aa22fac47c1691
leeminhhthee <minhthe22122003@gmail.com>
2025-03-30 23:11:05 +0700

    mongodb_aggregate

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -3,6 +3,7 @@ import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
 import ApiError from '~/utils/ApiError'
 import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
 
 const createNew = async (reqBody) => {
   try {
@@ -33,7 +34,20 @@ const getDetails = async (boardId) => {
       throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
     }
 
-    return board
+    // B1: Deep Clone board ra một cái mới để xử lý, không ảnh hưởng tới board ban đầu,
+    // tùy mục đích về sau mà có cần clone deep hay không
+    const resBoard = cloneDeep(board)
+
+    // B2: Đưa card về đúng column của nó
+    resBoard.columns.forEach(column => {
+      column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
+      // column.cards = resBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
+    })
+
+    // B3: Xoá mảng cards khỏi board ban đầu
+    delete resBoard.cards
+
+    return resBoard
   } catch (error) { throw error }
 }
 
