81dcfb9695d01a861edd596ac095b90b169a1078
leeminhhthee <minhthe22122003@gmail.com>
2025-04-05 00:32:35 +0700

    Update Whitelist Domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Khong can localhost nua thi o fie config/cors da luon cho phep moi truong dev
+  'https://trello-web-cyan.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
